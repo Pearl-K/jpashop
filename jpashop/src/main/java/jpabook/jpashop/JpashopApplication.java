dabe2edc8842300892237b4fc23de7f59013b936
Pearl <rkdwlswn227@gmail.com>
2023-12-06 18:32:37 +0900

    feat: view 강의

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -2,15 +2,11 @@ package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JpashopApplication {
-
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("Hello JPA!");
-		String data = hello.getData();
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
